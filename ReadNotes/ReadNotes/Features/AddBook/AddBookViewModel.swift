27783d1e5aeb0be61654c88fde591632ec4b7452
허신일 <shintwl@icloud.com>
2025-10-02 10:47:22 +0900

    feat: created AddBookViewModel

--- ReadNotes/ReadNotes/Features/AddBook/AddBookViewModel.swift
+++ ReadNotes/ReadNotes/Features/AddBook/AddBookViewModel.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+@MainActor
+final class AddBookViewModel: ObservableObject {
+    @Published var title = ""
+    @Published var author = ""
+    @Published var publisher = ""
+    @Published var isSaving = false
+    @Published var error: AppError?
+    
+    let repo: BookRepository
+    init(repo: BookRepository) { self.repo = repo }
+    
+    var canSave: Bool { !title.trimmingCharacters(in: .whitespaces).isEmpty }
+    
+    func save() async -> Bool {
+        guard canSave else { return false }
+        isSaving = true; defer { isSaving = false }
+        do {
+            let book = Book(title: title, author: author, publisher: publisher)
+            try await repo.add(book)
+            return true
+        } catch {
+            self.error = .wrap(error)
+            return false
+        }
+    }
+}
